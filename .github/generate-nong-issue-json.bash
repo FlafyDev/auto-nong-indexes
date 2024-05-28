1a363ad3a35e0ada20236c78c970b1fce190fe24
FlafyDev <flafyarazi@gmail.com>
2024-05-29 00:18:30 +0300

    feat: Add issue template for adding nongs and automatic merging

---
+++ .github/generate-nong-issue-json.bash
@@ -0,0 +1,28 @@
+resf="${HOME}/issue-parser-result.json" # Form result file for easy access
+
+case $(jq -r .source "$resf") in
+  "Youtube")
+    yt_id=$(jq -r .\"youtube-link\" "$resf" | grep -oP '(?<=youtu\.be/|v=|\/|^)[^&?/]+' | tail -1)
+    jq '{
+        name: .["song-name"],
+        artist: .["artist-name"],
+        source: "youtube",
+        "yt-id": "'"$yt_id"'",
+        songs: [(.["song-id"] | tonumber)]
+      }' "$resf" > result.json
+    cat result.json
+    ;;
+  "Direct file download")
+    jq '{
+        name: .["song-name"],
+        artist: .["artist-name"],
+        source: "host",
+        url: .["direct-link"],
+        songs: [(.["song-id"] | tonumber)]
+      }' "$resf" > result.json
+    ;;
+  *)
+    echo "Invalid source"
+    exit 1
+    ;;
+esac
