4ffcce815c233211e899918da76e95a1b849fb25
FlafyDev <flafyarazi@gmail.com>
2024-11-22 12:36:53 +0200

    feat: Add action to auto update sfh

--- generator/bin/generator.dart
+++ generator/bin/generator.dart
@@ -4,11 +4,6 @@ import 'package:generator/generator.dart';
 
 void main(List<String> arguments) async {
   await jsonToIndexFile(
-    File('../auto-nong-index.min.json'),
-    await covnertOfficialIndex(),
-  );
-
-  await jsonToIndexFile(
     File('../sfh-index.min.json'),
     await genSFHIndex(),
   );
