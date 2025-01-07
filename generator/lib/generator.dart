04cfffc41e1c83db4e5723d06e94bdceb6ea9f84
blu3 <blu3berry@disroot.org>
2025-01-07 14:15:54 +0300

    Reformat

--- generator/lib/generator.dart
+++ generator/lib/generator.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
-
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
@@ -11,11 +9,11 @@ Future<void> jsonToIndexFile(File file, Map<String, Object?> data) async {
   await file.writeAsString(JsonEncoder().convert(data));
 }
 
-Future<Map<String, Object?>> covnertOfficialIndex() async {
+Future<Map<String, Object?>> convertOfficialIndex() async {
   final res = await http.get(Uri.parse(
       'https://raw.githubusercontent.com/FlafyDev/auto-nong-indexes/main/official.json'));
-  final obj =
-      (json.decode(res.body) as List<dynamic>).cast<Map<String, dynamic>>();
+  final obj = (json.decode(res.body) as List<dynamic>)
+      .cast<Map<String, dynamic>>();
 
   final ytSongs = obj.fold<Map<String, dynamic>>({}, (acc, entry) {
     final ytID = entry['yt-id'];
@@ -32,8 +30,10 @@ Future<Map<String, Object?>> covnertOfficialIndex() async {
 
     final startOffset = entry['startOffset'] ?? 0;
 
-    final id = uuid.v5("8dd3aa81-28f2-40ca-afad-314ddd5caadd",
-        name + artist + songs.toString() + ytID + startOffset.toString());
+    final id = uuid.v5(
+      "8dd3aa81-28f2-40ca-afad-314ddd5caadd",
+      name + artist + songs.toString() + ytID + startOffset.toString(),
+    );
 
     return {
       ...acc,
@@ -60,17 +60,17 @@ Future<Map<String, Object?>> covnertOfficialIndex() async {
       "submit": {
         "supportedSongTypes": ["youtube"],
         "preSubmitMessage": """
-Pressing continue will redirect you to the GitHub issue page to submit your song request. It will be auto filled with the data you provided.
+Pressing continue will redirect you to the GitHub issue page to submit your song request. It will be auto-filled with the data you provided.
 
 **Submissions that do not respect the following rules will almost always be rejected**
 
-1. NONG replacement songs(not remixes/mashups) should only be accepted if the level that it was made for is judged very popular. Exceptions to this rule can be made if the replacement Newgrounds song that is used in the level is not used and not likely to be used by any level(For example, [Chilled 1](https://www.newgrounds.com/audio/listen/1)).
-2. Do not accept submissions that replace songs that are used by other popular levels, especially if one of these levels is rated. For example, do not replace Stereo Madness or Tennobyte - Fly Away with an unrelated song.
-3. Before accepting a submission, make sure all the fields(Name, Artist, etc) are written correctly
-4. **Test the submission in game** before submitting/accepting it. If it has a corresponding level then make sure it syncs with it. If it's a remix/mashup make sure it syncs with the replaced song. You can test in game by adding a new song and copying the Youtube ID from the submission.
-5. Links from Google Drive, Mediafire, Dropbox, etc are not allowed. Please only submit YouTube video links, or **permanent** direct download links from CDNs that has given permission for Auto Nong to download from. Right now there isn't any CDNs that are allowed. If you don't want to upload to YouTube then submit to the SFH. 
+1. NONG replacement songs (not remixes/mashups) should only be accepted if the level that it was made for is judged very popular. Exceptions to this rule can be made if the replacement Newgrounds song that is used in the level is not used and not likely to be used by any level (e.g., [Chilled 1](https://www.newgrounds.com/audio/listen/1)).
+2. Do not accept submissions that replace songs used by other popular levels, especially if one of these levels is rated. For example, do not replace Stereo Madness or Tennobyte - Fly Away with an unrelated song.
+3. Before accepting a submission, ensure all the fields (Name, Artist, etc.) are written correctly.
+4. **Test the submission in-game** before submitting/accepting it. If it has a corresponding level, ensure it syncs with it. If it's a remix/mashup, ensure it syncs with the replaced song. Test in-game by adding a new song and copying the YouTube ID from the submission.
+5. Links from Google Drive, Mediafire, Dropbox, etc., are not allowed. Only submit YouTube video links or **permanent** direct download links from CDNs that Auto Nong has permission to download from. Currently, no CDNs are allowed. If you don't want to upload to YouTube, submit to the SFH.
 
-Only Auto Nong Moderators and Beginner Moderators can accept or reject submissions. This is done by commenting "accept" or "reject" under a submission.
+Only Auto Nong Moderators and Beginner Moderators can accept or reject submissions by commenting "accept" or "reject" under a submission.
 """
             .trim(),
         "requestParams": {
@@ -89,8 +89,8 @@ Only Auto Nong Moderators and Beginner Moderators can accept or reject submissio
 
 Future<Map<String, Object?>> genSFHIndex() async {
   final res = await http.get(Uri.parse('https://api.songfilehub.com/songs'));
-  final obj =
-      (json.decode(res.body) as List<dynamic>).cast<Map<String, dynamic>>();
+  final obj = (json.decode(res.body) as List<dynamic>)
+      .cast<Map<String, dynamic>>();
 
   final hostedSongs = obj.fold<Map<String, dynamic>>({}, (acc, entry) {
     var songID = int.tryParse(entry['songID'] ?? "");
@@ -132,6 +132,7 @@ Future<Map<String, Object?>> genSFHIndex() async {
 
     final songFullName = entry['songName'];
     if (songFullName is! String) return acc;
+
     final String songName;
     final String artistName;
     if (songFullName.contains(" - ")) {
@@ -168,12 +169,12 @@ Future<Map<String, Object?>> genSFHIndex() async {
       "submit": {
         "supportedSongTypes": ["local", "youtube", "hosted"],
         "preSubmitMessage": """
-To submit your own song/s, please use the Song File Hub Discord Bot with the /Submit command to be reviewed by Helpers.
+To submit your own song(s), please use the Song File Hub Discord Bot with the /submit command to be reviewed by helpers.
 Please read the rules and walkthroughs for the commands before submitting!
 
 Only mp3/ogg files are supported.
 
-Disclaimer: It may take a bit to show on Jukebox's song list after being accepted. 
+Disclaimer: It may take a bit to show on Jukebox's song list after being accepted.
 """
             .trim(),
         "requestParams": {
